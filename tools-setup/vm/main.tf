73fa81b781bed856084e59a804fa2cfd009f7bf1
Prasanth Reddy <prasanthreddy1792@gmail.com>
2025-06-30 17:45:58 +0530

    learn devops

--- tools-setup/vm/main.tf
+++ tools-setup/vm/main.tf
@@ -3,6 +3,7 @@ resource "azurerm_public_ip" "publicip" {
   resource_group_name = var.rg_name
   location            = var.rg_location
   allocation_method   = "Dynamic"
+  sku                 = "Basic"
 }
 
 resource "azurerm_network_interface" "privateip" {
