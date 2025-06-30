ccb55c5a75b4193ce3860d18574d14f47c32a18d
Prasanth Reddy <prasanthreddy1792@gmail.com>
2025-06-30 17:47:55 +0530

    learn devops

--- tools-setup/vm/main.tf
+++ tools-setup/vm/main.tf
@@ -63,6 +63,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   admin_username       = "azuser"
   admin_password       = "Devops@123456"
   network_interface_ids = [azurerm_network_interface.privateip.id]
+  disable_password_authentication = true
 
   os_disk {
     name              = "${var.name}-disk"
