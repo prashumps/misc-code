7e5c2081c82c58d92e3cb54223cd52c994add5b2
Prasanth Reddy <prasanthreddy1792@gmail.com>
2025-07-03 23:17:51 +0530

    learn devops

--- vault-secrets/variables.tf
+++ vault-secrets/variables.tf
@@ -21,6 +21,16 @@ variable "values" {
         foo = "bar"
       }
     }
+    frontend = {
+      secret = "roboshop-dev"
+      value = {
+        catalogue_url = "http://catalogue-dev.prashumps.online:8080/"
+        user_url      = "http://user-dev.prashumps.online:8080/"
+        cart_url      = "http://cart-dev.prashumps.online:8080/"
+        shippinge_url = "http://shipping-dev.prashumps.online:8080/"
+        payment_url   = "http://payment-dev.prashumps.online:8080/"
+      }
+    }
     ssh = {
         secret = "roboshop-infra"
         value = {
