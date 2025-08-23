ffa18a6e51289a5287a29799be89003c2900a277
Prasanth Reddy <prasanthreddy1792@gmail.com>
2025-08-23 14:52:48 +0530

    learn devops

--- vault-secrets/variables.tf
+++ vault-secrets/variables.tf
@@ -45,7 +45,7 @@ variable "values" {
     shipping = {
       secret = "roboshop-dev"
       value = {
-        CART_ENDPOINT = "cart-dev.prashumps.online:8080"
+        CART_ENDPOINT = "cart:8080"
         DB_HOST = "mysql-dev.prashumps.online"
         DB_USER = "root"
         DB_PASS = "RoboShop@1"
